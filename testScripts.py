e77a68fadf644bd6de392e278ab4a2a1caf29b21
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2019-02-09 04:25:36 +0900

    現在複数のコンストレイントに挑戦中

--- testScripts.py
+++ testScripts.py
@@ -1,1 +1,10 @@
-from maya import cmds
+import maya.cmds as cmds
+import pymel.core as pm
+
+def main():
+    sel = pm.ls(sl = True, dag=True)
+
+    for jntname in sel:
+        if jntname.find('jnt') > -1:
+            print jntname
+            #pm.orientConstraint( , )
