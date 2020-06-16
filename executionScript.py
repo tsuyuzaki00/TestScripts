c072f46f023cd8341b23208f1b887bc10ba94034
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2020-06-17 03:20:27 +0900

    テンプレスクリプト作成

---
+++ executionScript.py
@@ -0,0 +1,4 @@
+from mainEdit import CornerEdgeSelect as ps
+reload(ps); ps.main()
+
+import pymel.core as pm
--- testScripts.py
+++ testScripts.py
@@ -2,9 +2,7 @@ import maya.cmds as cmds
 import pymel.core as pm
 
 def main():
-    sel = pm.ls(sl = True, dag=True)
+    sels = pm.ls(sl = True, dag=True)
 
-    for jntname in sel:
-        if jntname.find('jnt') > -1:
-            print jntname
-            #pm.orientConstraint( , )
+    for sel in sels:
+        print sel
