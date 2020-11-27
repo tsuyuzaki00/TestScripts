3bd05e916053dcf0333a34777da52b7fc0843e94
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2020-11-27 19:10:41 +0900

    サンプル用jsonとxmlの更新

--- jsonSample.py
+++ jsonSample.py
@@ -2,23 +2,25 @@ import json
 from collections import OrderedDict as od
 import pprint
 
-s = r'{"C": "\u3042", "A": {"i": 1, "j": 2}, "B": [{"X": 1, "Y": 10}, {"X": 2, "Y": 20}]}'
+#filePass = 'C:/Users/tsuyuzaki.tatsuya/Desktop/test/output.json'
+filePass = 'testScripts\jsonRun.json'
 
-print(s)
+def testWriteJson():
+    s = {"object": { "node" : [{ "name" : "pCube", "attrbute": [ {"string": "rotateX", "value": "0.0"},{"string": "rotateY", "value": "0.0"},{"string": "rotateZ", "value": "0.0"} ]}]}}
+    with open(filePass, 'w') as f:
+        json.dump(s, f, indent = 4, ensure_ascii =False)
 
-d = json.loads(s)
+def testReadJson():
+    with open(filePass, 'r') as f:
+        root = json.load(f)
+        #pprint.pprint(root, width=40)
+        node = root['object']['node']
+        for attr in node:
+            attr2 = attr['attrbute']
+            for string in attr2:
+                print attr.get('name')
+                print string.get('string')
+                print string.get('value')
 
-pprint.pprint(d, width=40)
-
-print(type(d))
-
-
-
-with open('testScripts/jsonRun.json') as f:
-    df = json.load(f)
-
-pprint.pprint(df, width=40)
-
-print(type(df))
-
-
+#testWriteJson()
+testReadJson()
