f62518cd3529b6c8af309224d1ad77ac5279b0ea
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2021-02-12 16:33:08 +0900

    mayaで簡易的に実行するスクリプトを修正

--- executionScript.py
+++ executionScript.py
@@ -1,4 +1,8 @@
-from mainEdit import skinPaintValue as ps
+''' LibraryReload '''
+#from MayaLibrary import setCurves as ml; reload(ml);
+''' Run '''
+#from mainEdit import skinPaintValue as ps
+from MayaExecute import setCurves_antenna as ps
 reload(ps); ps.main()
 
 #import pymel.core as pm
